c74f6c3b3a2f15ad73ce084d6e62250cd7e16d56
srinurathna58-pixel <srinurathna58@gmail.com>
2026-03-16 15:58:02 +0530

    Delete SQL directory

--- SQL/Data_Exploration/07_Conditional_aggregartion_analysis.sql
+++
@@ -1,5 +0,0 @@
--- High-cost vs low-cost warranty claims
-select region, sum(claim_count) as total_claims, sum(case when repair_cost > 5000 then claim_count else 0 end) as high_cost_claims,
-sum(case when repair_cost <= 5000 then claim_count else 0 end) as low_cost_claims, sum(repair_cost) as total_warranty_cost
-from warranty_data
-group by region;
